868ae8e5d0f36d84fb696b6ed19d4d826eabf073
pepanzdorf <melicharovo@gmail.com>
2024-06-26 19:35:18 +0200

    clean up main class

--- TablatureConverter/Classes/TabParser.cs
+++ TablatureConverter/Classes/TabParser.cs
@@ -14,7 +14,7 @@ public class TabParser
     private readonly IInstrumentTabBuilder _instrumentTabBuilder;
     private readonly TextWriter _tabOutput;
     private readonly int _transposeSemitones;
-    private StringBuilder _buffer = new StringBuilder();
+    private readonly StringBuilder _buffer = new StringBuilder();
     
     public TabParser(TextReader tabSource, TextWriter tabOutput,IInstrumentTabParser instrumentTabParser, IInstrumentTabBuilder instrumentTabBuilder, int transposeSemitones)
     {
@@ -29,16 +29,16 @@ public class TabParser
     {
         while (FindInstrumentTab())
         {
-            // We found a tabulature, now we need to parse it
-            // First we need to separate the tabulature from any extra stuff that might be after it
-            (string separatedTabulature, string extra) = SeparateTabulature(_buffer.ToString());
-            // Then we parse the tabulature
-            (List<MusicalPart> parsedTabulature, Note lowestNote) = _instrumentTabParser.Parse(separatedTabulature);
+            // We found a tablature, now we need to parse it
+            // First we need to separate the tablature from any extra stuff that might be after it
+            (string separatedTablature, string extra) = SeparateTablature(_buffer.ToString());
+            // Then we parse the tablature
+            (List<MusicalPart> parsedTablature, Note lowestNote) = _instrumentTabParser.Parse(separatedTablature);
             // Then we transpose it
-            Transpose(parsedTabulature);
+            Transpose(parsedTablature);
             // Then we build it and write it to output
-            _tabOutput.Write(_instrumentTabBuilder.Build(parsedTabulature, lowestNote));
-            // Then we write the extra stuff that was after the tabulature
+            _tabOutput.Write(_instrumentTabBuilder.Build(parsedTablature, lowestNote));
+            // Then we write the extra stuff that was after the tablature
             _tabOutput.WriteLine(extra);
             _buffer.Clear();
         }
@@ -48,7 +48,7 @@ public class TabParser
     {
         /*
          * Goes through the input file line by line and finds the next instrument tab.
-         * Parts of file that are not tabulature are written to output immediately.
+         * Parts of file that are not tablature are written to output immediately.
          */
         string? line;
         while ((line = _tabSource.ReadLine()) != null)
@@ -70,35 +70,35 @@ public class TabParser
         return false;
     }
     
-    private (string separatedTabulature, string extra) SeparateTabulature(string tabulature)
+    private (string separatedTablature, string extra) SeparateTablature(string tablature)
     {
         /*
-         * Sometimes people put extra stuff after the tabulature, like repetitions or explanations.
-         * We need to separate that from the tabulature so that it can be added back later.
+         * Sometimes people put extra stuff after the tablature, like repetitions or explanations.
+         * We need to separate that from the tablature so that it can be added back later.
          */
-        StringBuilder separatedTabulature = new StringBuilder();
+        StringBuilder separatedTablature = new StringBuilder();
         StringBuilder extra = new StringBuilder();
-        string[] lines = tabulature.Split('\n', StringSplitOptions.RemoveEmptyEntries);
-        int shortestLine = lines.OrderBy(s => s.Length).First().Length;
+        string[] lines = tablature.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        int shortestLine = lines.Min(s => s.Length);
         foreach (string line in lines)
         {
             if (line.Length > shortestLine)
             {
-                separatedTabulature.AppendLine(line[..shortestLine]);
+                separatedTablature.AppendLine(line[..shortestLine]);
                 extra.Append(line[shortestLine..] + " ");
             }
             else
             {
-                separatedTabulature.AppendLine(line);
+                separatedTablature.AppendLine(line);
             }
         }
-        return (separatedTabulature.ToString(), extra.ToString());
+        return (separatedTablature.ToString(), extra.ToString());
     }
     
-    private void Transpose(List<MusicalPart> tabulature)
+    private void Transpose(List<MusicalPart> tablature)
     {
-        // Transpose all the tabs in file
-        foreach (MusicalPart part in tabulature)
+        // Transpose all notes in tab
+        foreach (MusicalPart part in tablature)
         {
             foreach (IMusicalSymbol symbol in part.Symbols)
             {
